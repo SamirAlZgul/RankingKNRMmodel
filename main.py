e8a2e92f72bc3bb58db3792c70985d45431e9cdf
SamirAlZgul <samiralzgulfx@gmail.com>
2026-03-02 11:20:16 +0300

    Adding function _get_matching_matrix to main.py

---
+++ main.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn.functional as F
+
+class GaussianKernel(torch.nn.Module):
+    def __init__(self, mu=1., sigma=1.):
+        super().__init__()
+        self.mu=mu
+        self.sigma=sigma
+    def forward(self,x):
+        return torch.exp(-0.5*torch.pow(x-self.mu,2)/(self.sigma**2))
+
+class KNRM(torch.nn.Module):
+    """
+        KNRM (Kernel-based Neural Ranking Model) - нейросетевая модель для ранжирования документов
+        Использует ядра гаусса для моделирования совпадений на разных уровнях точности
+    """
+    def __init__(self, kernel_num=21, sigma=0.1, exact_sigma=0.001):
+        """
+            Args:
+            kernel_num: количество ядер (по умолчанию 21, как в оригинальной статье)
+            sigma: стандартное отклонение для ядер Гаусса
+        """
+        super(KNRM, self).__init__()
+        self.kernel_num = kernel_num
+        self.sigma = sigma
+        self.exact_sigma = exact_sigma
+
+
+    def _get_kernels_layers(self):
+        """
+            Создает слои с ядрами Гаусса для KNRM модели.
+            Каждое ядро отвечает за определенный уровень совпадения термов.
+            # активируем 21 степень похожести
+        """
+        # ModuleList позволяет корректно зарегистрировать все ядра как параметры модели
+        kernels = torch.nn.ModuleList()
+        for i in range(self.kernel_num):
+            mu = 1/(self.kernel_num-1) + 2*i/(self.kernel_num-1) - 1.0
+            sigma = self.sigma
+            if mu>1:
+                mu=1
+                sigma=self.exact_sigma
+            kernels.append(GaussianKernel(mu, sigma))
+        return kernels
+
+    def _get_matching_matrix(self, query, doc):
+        """
+        показывает как каждое слово запроса соотносится с каждым словом документа
+        на вход тут BWD - это матрица (Batch, Words, Dimension) -
+        (размер батча (количество запросов), количество слов в запросе, размер эмбеддинга)
+        BRD - это матрица (Batch, Words in Document, Dimension) -
+        (размер батча (количество документов), количество слов в док, размер эмбеддинга)
+        BWR - это матрица (Batch, Words, количество слов в док)
+        matching_matrix[b, i, j] =
+        косинусное сходство между i-тым словом запроса и j-тым словом документа в b-том примере
+        (используем построение матрицы в нотации Эйнштейна)
+        """
+        embed_query = self.embedding(query)
+        embed_doc = self.embedding(doc)
+        matching_matrix = torch.einsum(
+            'BWD, BRD->BWR',
+            F.normalize(embed_query, p=2, dim=-1),
+            F.normalize(embed_doc, p=2, dim=-1)
+        )
+
